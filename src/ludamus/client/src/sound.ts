002d2c101933c4b9b127e79709e716f9da3a0fdd
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-07-14 00:13:15 +0200

    fix(sound): use toggle cue for radio inputs

--- src/ludamus/client/src/sound.ts
+++ src/ludamus/client/src/sound.ts
@@ -71,7 +71,7 @@ const init = (): void => {
   /** Update the pref first so turning sound on is audible and turning it off isn't. */
   document.addEventListener("change", ({ target }) => {
     if (!(target instanceof HTMLInputElement)) return;
-    if (target.type === "radio") play("press");
+    if (target.type === "radio") play("toggle");
     if (target.type !== "checkbox") return;
     if (target.matches("[data-sound-toggle]")) setEnabled(target.checked);
     play("toggle");
